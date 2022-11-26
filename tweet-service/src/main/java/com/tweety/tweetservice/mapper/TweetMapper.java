e53b4602a781ef604fc7919e6c2a6736b889e531
ghmaimon <kaixokoko123@gmail.com>
2022-11-26 22:17:55 +0100

    TWEET-6: add create tweet endpoint

---
+++ tweet-service/src/main/java/com/tweety/tweetservice/mapper/TweetMapper.java
@@ -0,0 +1,10 @@
+package com.tweety.tweetservice.mapper;
+
+import com.tweety.tweetservice.dto.CreateTweetDto;
+import com.tweety.tweetservice.model.Tweet;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TweetMapper {
+    Tweet createTweetDtoToTweet(CreateTweetDto dto);
+}
